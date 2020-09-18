73ef040f0bb772d7d7ab78e48b74f6251c6beb3b
ThanhhNgo <ngomanhcongthanh@gmail.com>
2020-09-18 20:29:56 +0700

    alo lel el

--- models/SinhVien.js
+++ models/SinhVien.js
@@ -7,8 +7,12 @@ var SinhVien = function (masv,tensv,email,diemtoan,diemly,diemhoa,diemrenluyen,l
     this.diemHoa = diemhoa;
     this.diemRenLuyen = diemrenluyen;
     this.loaiSinhVien = loaisinhvien;
+    this.xepLoai = function () {
+        return 'Bét';
+    }
     this.tinhDiemTrungBinh = function () {
         var dtb = (Number(this.diemToan) + Number(this.diemLy) + Number(this.diemHoa)) /3;
         return dtb;
     }
+    this.name = "abababba";
 }
